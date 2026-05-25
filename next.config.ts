6a750b7ae5df960bce771d3c8d6923f2f78a5ebc
deeps-dpk <deeps.dpk@gmail.com>
2026-05-25 16:07:40 +0530

    modified next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Enables static export
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  // Use your repo name if your URL is https://<username>.github.io/<repo-name>/
+  // basePath: '/your-repo-name', 
 };
 
-export default nextConfig;
+module.exports = nextConfig;
